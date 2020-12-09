c9b562721011573d3b56719d5a0f5ada9b457e1c
Gustavo Barros Bastos Nomura <gustavo.nomura@PPM-SPO-8139.local>
2020-12-09 00:27:08 -0300

    base readme + helth check

---
+++ README.md
@@ -0,0 +1,8 @@
+go-money-api
+
+Este projeto tem duas funções básicas, me ajudar nos estudos de golang e arquitetura de projetos e melhorar a qualidade do gerenciamento dos meus investimentos.
+
+run
+docker-compose -f deployments/docker-compose.yml up
+
+go run cmd/go-money-api/main.go
---
+++ cmd/go-money-api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "ok")
+	})
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8085", nil))
+}
