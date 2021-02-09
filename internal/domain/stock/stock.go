79fd8fa5bdbcf85832925ecac4375f19327f5e66
Gustavo Barros Bastos Nomura <gustavo.nomura@ip-10-210-129-93.ec2.internal>
2021-02-09 15:23:13 -0300

    fiz json encode fields

--- internal/domain/stock/stock.go
+++ internal/domain/stock/stock.go
@@ -1,7 +1,8 @@
 package stock
 
+//Entity is the entity of the stock domain
 type Entity struct {
-	ID          int64  `json:id`
-	Code        string `json:code`
-	FantasyName string `fantasyName`
+	ID          int64  `json:"id"`
+	Code        string `json:"code"`
+	FantasyName string `json:"fantasyName"`
 }
