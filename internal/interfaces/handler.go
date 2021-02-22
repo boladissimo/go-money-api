1e0db0141c8c05d8472a3d5537ab03a1d572bbb4
Gustavo Barros Bastos Nomura <gustavo.nomura@PPM-SPO-8139.Dlink>
2021-02-22 13:37:47 -0300

    removing discovery

--- internal/interfaces/handler.go
+++ internal/interfaces/handler.go
@@ -2,10 +2,8 @@ package interfaces
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
-	"github.com/boladissimo/go-money-api/internal/infrastructure/util"
 	"github.com/boladissimo/go-money-api/internal/stocks"
 	"github.com/gorilla/mux"
 )
@@ -17,21 +15,6 @@ func GetRouter(stockController stocks.Controller) *mux.Router {
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "ok")
-	})
-
-	r.HandleFunc("/discover/{path}", func(w http.ResponseWriter, r *http.Request) {
-		files, err := ioutil.ReadDir(mux.Vars(r)["path"])
-		if err != nil {
-			util.LogError(err)
-		}
-
-		for _, file := range files {
-			fileFullName := file.Name()
-			fmt.Fprintf(w, fileFullName)
-		}
-
-		w.WriteHeader(http.StatusOK)
-
 	})
 
 	r.HandleFunc("/stock", stockController.GetAll).Methods(http.MethodGet)
